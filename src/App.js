469b09d8a029f41bb2f9835e7bfe6aa64ef5a787
Elizar Garcia <garciaelizar@yahoo.com>
2023-03-10 15:31:53 -0800

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Game from "./components/GameComponent/GameComponent";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Background" >
+    <h1 className="Title" >Tic Tac Toe</h1>
+     <Game />
     </div>
   );
 }
+
+
 
 export default App;
---
+++ src/components/SquareComponent/SquareComponent.js
@@ -0,0 +1,11 @@
+import react from 'react';
+
+export default function Square({ value, onSquareClick }) {
+    return (
+      <button className="square" onClick={onSquareClick}>
+        {value}
+      </button>
+    );
+  }
+
+  
